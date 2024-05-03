b0aa9c288e1fee0f02d028ddd9dcbd9dc897b780
Joe Watt <joewatt95@u.nus.edu>
2024-05-03 18:44:39 +0800

    format

--- lean/LoVe/LoVe03_BackwardProofs_Demo.lean
+++ lean/LoVe/LoVe03_BackwardProofs_Demo.lean
@@ -31,11 +31,10 @@ Syntax of tactical proofs:
 The keyword `by` indicates to Lean the proof is tactical. -/
 
 theorem fst_of_two_props :
-  ∀a b : Prop, a → b → a :=
-  by
-    intro a b
-    intro ha _hb
-    apply ha
+  ∀a b : Prop, a → b → a := by
+  intro a b
+  intro ha _hb
+  apply ha
 
 /- Note that `a → b → a` is parsed as `a → (b → a)`.
 
@@ -53,16 +52,15 @@ from the goal's conclusion (after `⊢`) into the goal's hypotheses (before `⊢
 theorem and adds the theorem's hypotheses as new goals. -/
 
 theorem fst_of_two_props_params (a b : Prop) (ha : a) (_hb : b) :
-  a :=
-  by apply ha
+  a := by
+  apply ha
 
 theorem prop_comp (a b c : Prop) (hab : a → b) (hbc : b → c) :
-  a → c :=
-  by
-    intro ha
-    apply hbc
-    apply hab
-    apply ha
+  a → c := by
+  intro ha
+  apply hbc
+  apply hab
+  apply ha
 
 /- The above proof step by step:
 
@@ -76,55 +74,55 @@ the goal. We can often use `apply` in such situations, but `exact` communicates
 our intentions better. -/
 
 theorem fst_of_two_props_exact (a b : Prop) (ha : a) (_hb : b) :
-  a :=
-  by exact ha
+  a := by
+  exact ha
 
 /- `assumption` finds a hypothesis from the local context that matches the
 goal's conclusion and applies it to prove the goal. -/
 
 theorem fst_of_two_props_assumption (a b : Prop)
     (ha : a) (_hb : b) :
-  a :=
-  by assumption
+  a := by
+  assumption
 
 /- `rfl` proves `l = r`, where the two sides are syntactically equal up to
 computation. Computation means unfolding of definitions, β-reduction
 (application of `fun` to an argument), `let`, and more. -/
 
 theorem α_example {α β : Type} (f : α → β) :
-  (fun x ↦ f x) = (fun y ↦ f y) :=
-  by rfl
+  (fun x ↦ f x) = (fun y ↦ f y) := by
+  rfl
 
 theorem β_example {α β : Type} (f : α → β) (a : α) :
-  (fun x ↦ f x) a = f a :=
-  by rfl
+  (fun x ↦ f x) a = f a := by
+  rfl
 
 def double (n : ℕ) : ℕ :=
   n + n
 
 theorem δ_example :
-  double 5 = 5 + 5 :=
-  by rfl
+  double 5 = 5 + 5 := by
+  rfl
 
 /- `let` introduces a definition that is locally scoped. Below, `n := 2` is only
 in scope in the expression `n + n`. -/
 
 theorem ζ_example :
   (let n : ℕ := 2
-   n + n) = 4 :=
-  by rfl
+   n + n) = 4 := by
+  rfl
 
 theorem η_example {α β : Type} (f : α → β) :
-  (fun x ↦ f x) = f :=
-  by rfl
+  (fun x ↦ f x) = f := by
+  rfl
 
 /- `(a, b)` is the pair whose first component is `a` and whose second component
 is `b`. `Prod.fst` is a so-called projection that extracts the first component
 of a pair. -/
 
 theorem ι_example {α β : Type} (a : α) (b : β) :
-  Prod.fst (a, b) = a :=
-  by rfl
+  Prod.fst (a, b) = a := by
+  rfl
 
 
 /- ## Reasoning about Logical Connectives and Quantifiers
@@ -158,14 +156,13 @@ Introduction rules: -/
 `p → False`. -/
 
 theorem And_swap (a b : Prop) :
-  a ∧ b → b ∧ a :=
-  by
-    intro hab
-    apply And.intro
-    apply And.right
-    exact hab
-    apply And.left
-    exact hab
+  a ∧ b → b ∧ a := by
+  intro hab
+  apply And.intro
+  apply And.right
+  exact hab
+  apply And.left
+  exact hab
 
 /- The above proof step by step:
 
@@ -181,12 +178,11 @@ fully prove it. In the proof below, `{ … }` is used for each of the two subgoa
 to give more structure to the proof. -/
 
 theorem And_swap_braces :
-  ∀a b : Prop, a ∧ b → b ∧ a :=
-  by
-    intro a b hab
-    apply And.intro
-    { exact And.right hab }
-    { exact And.left hab }
+  ∀a b : Prop, a ∧ b → b ∧ a := by
+  intro a b hab
+  apply And.intro
+  { exact And.right hab }
+  { exact And.left hab }
 
 /- Notice above how we pass the hypothesis `hab` directly to the theorems
 `And.right` and `And.left`, instead of waiting for the theorems' assumptions to
@@ -196,38 +192,34 @@ proof. -/
 opaque f : ℕ → ℕ
 
 theorem f5_if (h : ∀n : ℕ, f n = n) :
-  f 5 = 5 :=
-  by exact h 5
+  f 5 = 5 := by
+  exact h 5
 
 theorem Or_swap (a b : Prop) :
-  a ∨ b → b ∨ a :=
-  by
-    intro hab
-    apply Or.elim hab
-    { intro ha
-      exact Or.inr ha }
-    { intro hb
-      exact Or.inl hb }
+  a ∨ b → b ∨ a := by
+  intro hab
+  apply Or.elim hab
+  { intro ha
+    exact Or.inr ha }
+  { intro hb
+    exact Or.inl hb }
 
 theorem modus_ponens (a b : Prop) :
-  (a → b) → a → b :=
-  by
-    intro hab ha
-    apply hab
-    exact ha
+  (a → b) → a → b := by
+  intro hab ha
+  apply hab
+  exact ha
 
 theorem Not_Not_intro (a : Prop) :
-  a → ¬¬ a :=
-  by
-    intro ha hna
-    apply hna
-    exact ha
+  a → ¬¬ a := by
+  intro ha hna
+  apply hna
+  exact ha
 
 theorem Exists_double_iden :
-  ∃n : ℕ, double n = n :=
-  by
-    apply Exists.intro 0
-    rfl
+  ∃ n : ℕ, double n = n := by
+  apply Exists.intro 0
+  rfl
 
 
 /- ## Reasoning about Equality -/
@@ -241,35 +233,32 @@ theorem Exists_double_iden :
 
 theorem Eq_trans_symm {α : Type} (a b c : α)
     (hab : a = b) (hcb : c = b) :
-  a = c :=
-  by
-    apply Eq.trans
-    { exact hab }
-    { apply Eq.symm
-      exact hcb }
+  a = c := by
+  apply Eq.trans
+  { exact hab }
+  { apply Eq.symm
+    exact hcb }
 
 /- `rw` applies a single equation as a left-to-right rewrite rule, once. To
 apply an equation right-to-left, prefix its name with `←`. -/
 
 theorem Eq_trans_symm_rw {α : Type} (a b c : α)
     (hab : a = b) (hcb : c = b) :
-  a = c :=
-  by
-    rw [hab]
-    rw [hcb]
+  a = c := by
+  rw [hab]
+  rw [hcb]
 
 /- `rw` can expand a definition. Below, `¬¬ a` becomes `¬ a → False`, and `¬ a`
 becomes `a → False`. -/
 
 theorem a_proof_of_negation (a : Prop) :
-  a → ¬¬ a :=
-  by
-    rw [Not]
-    rw [Not]
-    intro ha
-    intro hna
-    apply hna
-    exact ha
+  a → ¬¬ a := by
+  rw [Not]
+  rw [Not]
+  intro ha
+  intro hna
+  apply hna
+  exact ha
 
 /- `simp` applies a standard set of rewrite rules (the __simp set__)
 exhaustively. The set can be extended using the `@[simp]` attribute. Theorems
@@ -278,15 +267,15 @@ can be temporarily added to the simp set with the syntax
 
 theorem cong_two_args_1p1 {α : Type} (a b c d : α)
     (g : α → α → ℕ → α) (hab : a = b) (hcd : c = d) :
-  g a c (1 + 1) = g b d 2 :=
-  by simp [hab, hcd]
+  g a c (1 + 1) = g b d 2 := by
+  simp [hab, hcd]
 
 /- `ac_rfl` is similar to `rfl`, but it can reason up to associativity and
 commutativity of `+`, `*`, and other binary operators. -/
 
 theorem abc_Eq_cba (a b c : ℕ) :
-  a + b + c = c + b + a :=
-  by ac_rfl
+  a + b + c = c + b + a := by
+  ac_rfl
 
 
 /- ## Proofs by Mathematical Induction
@@ -295,32 +284,28 @@ theorem abc_Eq_cba (a b c : ℕ) :
 named subgoal per constructor. -/
 
 theorem add_zero (n : ℕ) :
-  add 0 n = n :=
-  by
-    induction n with
-    | zero       => rfl
-    | succ n' ih => simp [add, ih]
+  add 0 n = n := by
+  induction n with
+  | zero       => rfl
+  | succ n' ih => simp [add, ih]
 
 theorem add_succ (m n : ℕ) :
-  add (.succ m) n = .succ (add m n) :=
-  by aesop
-    -- induction n with
-    -- | zero       => rfl
-    -- | succ n' ih => simp [add, ih]
+  add (.succ m) n = .succ (add m n) := by
+  induction n with
+  | zero       => rfl
+  | succ n' ih => simp only [add, ih]
 
 theorem add_comm (m n : ℕ) :
-  add m n = add n m :=
-  by aesop
-    -- induction n with
-    -- | zero       => simp [add, add_zero]
-    -- | succ n' ih => simp [add, add_succ, ih]
+  add m n = add n m := by
+  induction n with
+  | zero       => simp only [add, add_zero]
+  | succ n' ih => simp only [add, add_succ, ih]
 
 theorem add_assoc (l m n : ℕ) :
-  add (add l m) n = add l (add m n) :=
-  by
-    induction n with
-    | zero       => rfl
-    | succ n' ih => simp [add, ih]
+  add (add l m) n = add l (add m n) := by
+  induction n with
+  | zero       => rfl
+  | succ n' ih => simp only [add, ih]
 
 /- `ac_rfl` is extensible. We can register `add` as a commutative and
 associative operator using the type class instance mechanism (explained in
@@ -333,13 +318,12 @@ instance Commutative_add : Std.Commutative add where
   comm := add_comm
 
 theorem mul_add (l m n : ℕ) :
-  mul l (add m n) = add (mul l m) (mul l n) :=
-  by
-    induction n with
-    | zero       => rfl
-    | succ n' ih =>
-      simp only [mul, ih, SorryTheorems.add_add]
-      -- ac_rfl
+  mul l (add m n) = add (mul l m) (mul l n) := by
+  induction n with
+  | zero       => rfl
+  | succ n' ih =>
+    simp only [mul, ih, SorryTheorems.add_add]
+    -- ac_rfl
 
 /- ## Cleanup Tactics
 
@@ -349,13 +333,12 @@ theorem mul_add (l m n : ℕ) :
 
 theorem cleanup_example (a b c : Prop) (ha : a) (hb : b)
     (hab : a → b) (hbc : b → c) :
-  c :=
-  by
-    clear ha hab a
-    apply hbc
-    clear hbc c
-    rename b => h
-    exact h
+  c := by
+  clear ha hab a
+  apply hbc
+  clear hbc c
+  rename b => h
+  exact h
 
 end BackwardProofs
 
