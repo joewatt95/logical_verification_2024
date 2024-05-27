e57b2c8d6765c656ca943dcb2cde424ea1879346
joewatt95 <joewatt95@u.nus.edu>
2024-05-27 16:14:49 +0800

    bump lean to v4.8.0-rc2

--- lean/lakefile.lean
+++ lean/lakefile.lean
@@ -15,18 +15,18 @@ lean_lib LoVe where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4"
-    @ "f3ca78288e785e4c4219ab0e6a88af33dcb5b85f"
+    @ "b0eb4bbc7ec80e6be10133c9be693245d3461d26"
 
 require auto from git
   "https://github.com/leanprover-community/lean-auto"
-    @ "3a2c4c94bf14a8596ff98e81f608d0886f4b559a"
+    @ "91cd0e81ec8bd16baa2c08e3d00a7f8e473477b4"
 
 require Duper from git
-  "https://github.com/leanprover-community/duper" @ "v0.0.11"
+  "https://github.com/leanprover-community/duper" @ "v0.0.12"
 
 require LeanCopilot from git
-  "https://github.com/lean-dojo/LeanCopilot" @ "v1.2.0"
+  "https://github.com/lean-dojo/LeanCopilot" @ "v1.2.2"
 
 require loogle from git
   "https://github.com/nomeata/loogle"
-    @ "34713b27a0cab653313288397c5f0efb6b2061b0"
+    @ "fa2ddf5771cc25b0d6e552ef63b51a68351e437f"
