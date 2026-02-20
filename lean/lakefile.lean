de5bbe234c8694a46615612ef6bd2fd6278af6b7
joewatt95 <joewatt95@u.nus.edu>
2026-02-20 11:50:49 +0800

    bump

--- lean/lakefile.lean
+++ lean/lakefile.lean
@@ -8,22 +8,23 @@ lean_lib LoVe where
   roots := #[`LoVe]
   globs := #[Glob.submodules `LoVe]
 
--- require "Joewatt95" / smt from git
---   "https://github.com/Joewatt95/lean-smt" @
---   "e8a675569cd2fcd4b916ff173aff8a17d7682718"
+require "ufmg-smite" / smt @ git
+  "3bc19f2d3caba4c5fbfe213143c79364c3d9c97a"
 
-require "chasenorman" / Canonical @ git "v4.26.0"
+require "chasenorman" / Canonical @ git "v4.27.0"
 
-require "JOSHCLUNE" / Hammer @ git "v4.26.0"
+require "JOSHCLUNE" / Hammer @ git "v4.27.0"
 
 require "Joewatt95" / egg from git
   "https://github.com/Joewatt95/lean-egg" @
-  "cff704447cfdb85126d16557a9ec050b27e33b64"
+  "8b978c7e1614dc88b1165d0543141412f8631e29"
 
 require "nomeata" / calcify @
   git "b89b823f26eb35a1d9ed57af2663128d6b3a35c2"
 
 require "nomeata" / loogle @
-  git "0ac13cd97d030b989151359429f9061c12faf0f6"
+  git "79343e3e37b64046e6b555936682012e80300df1"
 
-require "leanprover-community" / mathlib @ git "v4.26.0"
+require "leanprover" / verso @ git "v4.27.0"
+
+require "leanprover-community" / mathlib @ git "v4.27.0"
